29a6e1e3b3c075dcd382d4264e857efb80acb627
witusj <wfth69@gmail.com>
2021-07-02 22:36:26 +0200

    Minor

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -100,7 +100,7 @@ const Page = ({ user }) => {
         Je hebt nu een definitie van het begrip Smart Industry. Tijdens de minor gaan we hier gezamenlijk een persoonlijke invulling aan geven. Dit doen we door puzzelstukjes te verzamelen (kennis, indrukken, ervaringen, tests en demo's, etc.). Deze koppelen we aan elkaar en zo ontstaat ons eigen Smart Industry verhaal.
       </p>
       <p>
-        Het eerst puzzelstukje vind je hieronder. Deze krijg je van ons - een cadeautje. Maar let op! Vanaf nu verwachten we dat je zelf puzzelstukjes gaat verzamelen en aan elkaar knoopt. Dit noemen we je Smart Journey. Zoals een echte puzzel heeft je Smart Journey geen begin en geen einde. Het onderstaande kan een hoekstukje zijn of het midden en je kunt kiezen om eerst de randjes te doen of juist van binnen naar buiten te werken.
+        Het eerst puzzelstukje vind je hieronder. Deze krijg je van ons - een cadeautje. Maar let op! Vanaf nu verwachten we dat je zelf puzzelstukjes gaat verzamelen en aan elkaar knoopt. Dit noemen we de Smart Journey. Zoals een echte puzzel heeft jouw Smart Journey geen begin en geen einde. Het onderstaande kan een hoekstukje zijn of het midden en je kunt kiezen om eerst de randjes te doen of juist van binnen naar buiten te werken.
       </p>
       <p>
         Jouw opdracht luidt om de eerste koppelingen te gaan leggen. Wat is de betekenis van jouw stukje voor het grotere geheel? Welke relaties met andere elementen binnen Smart Industry kun je ontdekken? Werk een Smart Industry story uit en neem die mee naar onze eerste ontmoeting.
