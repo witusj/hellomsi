3f074b5e025dfae29276c2ad490532cf3eadfe55
witusj <wfth69@gmail.com>
2021-07-02 21:43:17 +0200

    Minor

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -40,43 +40,47 @@ const Page = ({ user }) => {
       <p>
         Hieronder vind je algemene informatie en de invulling van de eerste week. De roostering van de overige weken is helaas pas na de zomervakantie bekend.
       </p>
-      <p>
-        De naam van je klas is BKN-M06.
-      </p>
-      <p>
+      <ol>
 
-        We starten op woensdag 1 september 9.30 uur in de Meshallen, Nieuweweg 240, 6603 BV Wijchen. Neem je laptop mee. We helpen je dan ook met de installatie van de software op je computer (zie punt 10).
-        Op donderdag 2 september worden er ook gedurende de hele dag workshops verzorgd, 's ochtend online en 's middags in de Meshallen (je bent wél de hele dag welkom in de Meshallen). Op vrijdagochtend 3 september is er een workshop in de Meshallen.
-      </p>
-      <p>
-        De eerste 6 weken zijn er vanuit de 4 leerlijnen, workshops gepland op zo’n 3 dagen per week. De workshops gaan over Smart Business, Smart Connection, Smart Technology en Smart Start (o.a. research). Deze 6 weken ben je intensief aan het studeren (zo’n 32 uur, afhankelijk van je voorkennis en talent). In deze weken toon je aan dat je gemotiveerd en geschikt bent om voor één van onze opdrachtgevers met een project aan de slag te gaan. In je rooster zie je deze leerlijnen als volgt terug: MSI1A.9: Smart Start, MSI2A.9: Smart Connection, MSI3A.9: Smart Technology, MSI4A.9: Smart Business en MSI5A.9: Smart Project.
-      </p>
-      <p>
-        Op woensdagochtend 15 september 2021 maak je kennis met de opdrachtgevers en hun vraagstukken.
-        Na 6 weken workshops, dus vanaf maandag 11 oktober, ga je in groepen van gemiddeld 3 studenten voor een periode van 12 weken een opdracht doen bij een bedrijf. We verwachten dat je daar 4 dagen in de week aan werkt en 1 dag per week zal er nog een workshop gegeven worden, kan er een bedrijfsbezoek plaatsvinden en is er ruimte voor begeleiding/ intervisie.
-      </p>
-      <p>
-        De herfstvakantie is van 25 tot en met 29 oktober.
-      </p>
-      <p>
-        Door te werken aan het project en je voor te bereiden op en actief deel te nemen aan de trainingen, workshops, bedrijfsbezoeken, intervisies e.d. kun je aantonen dat je je ontwikkeld hebt en voldoet aan de competenties. In een smart portfolio (website) leg je je bewijzen vast. Zonder bewijs kunnen we de competenties niet aftoetsen en dus geen studiepunten verlenen.
-      </p>
-      <p>
-        Moodle is de leeromgeving van de HAN. Je kunt een account aanmaken en je verdiepen in de leeromgeving van MSI (deze is nog wel onder constructie).
-      </p>
-      <p>
-        Je hebt een laptop nodig. Neem die altijd mee en zorg dat je de juiste software hebt geïnstalleerd (zie onderstaande instructies en gedurende het traject).
-        Voor het onderdeel Big Data dien je (gratis) software te installeren: Visual Studio Code. Instructies vind je hier.
-      </p>
-      <p>
-        Maak een account aan bij Github en stuur je usernaam door aan Witek ten Hove (witek.tenhove@han.nl).
-      </p>
-      <p>
-        Mocht je je alvast willen verdiepen in de onderwerpen die behandeld zullen worden, lees bijvoorbeeld over IoT, VR/ AR, big data e.d. via McKinsey, HBR, forbes, gartner.com, etc. Meer over AI lees je via deze link.
-      </p>
-      <p>
-        En een student van een vorige lichting (Benno Schuuring) heeft op LinkedIn een alumni groep aangemaakt. Meld je hier aan.
-      </p>
+
+        <li>
+          De naam van je klas is BKN-M06.
+        </li>
+        <li>
+
+          We starten op woensdag 1 september 9.30 uur in de Meshallen, Nieuweweg 240, 6603 BV Wijchen. Neem je laptop mee. We helpen je dan ook met de installatie van de software op je computer (zie punt 10).
+          Op donderdag 2 september worden er ook gedurende de hele dag workshops verzorgd, 's ochtend online en 's middags in de Meshallen (je bent wél de hele dag welkom in de Meshallen). Op vrijdagochtend 3 september is er een workshop in de Meshallen.
+        </li>
+        <li>
+          De eerste 6 weken zijn er vanuit de 4 leerlijnen, workshops gepland op zo’n 3 dagen per week. De workshops gaan over Smart Business, Smart Connection, Smart Technology en Smart Start (o.a. research). Deze 6 weken ben je intensief aan het studeren (zo’n 32 uur, afhankelijk van je voorkennis en talent). In deze weken toon je aan dat je gemotiveerd en geschikt bent om voor één van onze opdrachtgevers met een project aan de slag te gaan. In je rooster zie je deze leerlijnen als volgt terug: MSI1A.9: Smart Start, MSI2A.9: Smart Connection, MSI3A.9: Smart Technology, MSI4A.9: Smart Business en MSI5A.9: Smart Project.
+        </li>
+        <li>
+          Op woensdagochtend 15 september 2021 maak je kennis met de opdrachtgevers en hun vraagstukken.
+          Na 6 weken workshops, dus vanaf maandag 11 oktober, ga je in groepen van gemiddeld 3 studenten voor een periode van 12 weken een opdracht doen bij een bedrijf. We verwachten dat je daar 4 dagen in de week aan werkt en 1 dag per week zal er nog een workshop gegeven worden, kan er een bedrijfsbezoek plaatsvinden en is er ruimte voor begeleiding/ intervisie.
+        </li>
+        <li>
+          De herfstvakantie is van 25 tot en met 29 oktober.
+        </li>
+        <li>
+          Door te werken aan het project en je voor te bereiden op en actief deel te nemen aan de trainingen, workshops, bedrijfsbezoeken, intervisies e.d. kun je aantonen dat je je ontwikkeld hebt en voldoet aan de competenties. In een smart portfolio (website) leg je je bewijzen vast. Zonder bewijs kunnen we de competenties niet aftoetsen en dus geen studiepunten verlenen.
+        </li>
+        <li>
+          Moodle is de leeromgeving van de HAN. Je kunt een account aanmaken en je verdiepen in de leeromgeving van MSI (deze is nog wel onder constructie).
+        </li>
+        <li>
+          Je hebt een laptop nodig. Neem die altijd mee en zorg dat je de juiste software hebt geïnstalleerd (zie onderstaande instructies en gedurende het traject).
+          Voor het onderdeel Big Data dien je (gratis) software te installeren: Visual Studio Code. Instructies vind je hier.
+        </li>
+        <li>
+          Maak een account aan bij Github en stuur je usernaam door aan Witek ten Hove (witek.tenhove@han.nl).
+        </li>
+        <li>
+          Mocht je je alvast willen verdiepen in de onderwerpen die behandeld zullen worden, lees bijvoorbeeld over IoT, VR/ AR, big data e.d. via McKinsey, HBR, forbes, gartner.com, etc. Meer over AI lees je via deze link.
+        </li>
+        <li>
+          Een student van een vorige lichting (Benno Schuuring) heeft op LinkedIn een alumni groep aangemaakt. Meld je hier aan.
+        </li>
+      </ol>
       <p>
         Geniet van de zomer en we verheugen ons erop om je op 1 september in Wijchen te ontmoeten!
       </p>
